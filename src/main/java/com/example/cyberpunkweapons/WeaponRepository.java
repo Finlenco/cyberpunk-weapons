00e1cc1268fcb496574c4bfb243a1ab0774202a6
Finlenco <2095650349@qq.com>
2025-06-07 14:03:28 +0800

    Update Dockerfile and resolve README.md conflict

--- src/main/java/com/example/cyberpunkweapons/WeaponRepository.java
+++ src/main/java/com/example/cyberpunkweapons/WeaponRepository.java
@@ -2,7 +2,7 @@ package com.example.cyberpunkweapons;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+ 
 @Repository
 public interface WeaponRepository extends JpaRepository<Weapon, Long> {
 } 
